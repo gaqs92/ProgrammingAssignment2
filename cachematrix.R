c4a51e1a89ae2767c250c776a39d676f0b429587
gaqs92 <gaqs92@gmail.com>
2020-11-09 13:57:53 -0400

    commit cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,32 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function(){x}
+  set_inv <- function(invcal) {inv <<- invcal}
+  get_inv <- function() {inv}
+  list(set = set, get = get,
+       set_inv = set_inv,
+       get_inv = get_inv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  inv <- x$get_inv()
+  if (!is.null(inv)) {
+    message("getting catched data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$set_inv(inv)
+  inv
         ## Return a matrix that is the inverse of 'x'
 }
+
